34aa9b9cd1ec34144fe1cc142e86722a5090b14c
Jemima Siddiqui <jemimasiddiqui1@gmail.com>
2022-06-15 17:01:10 +1000

    added clickable buttons to take users to deployed app

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+const work1 = document.getElementById("btn-1"); 
+const work2 = document.getElementById("btn-2"); 
+const work3 = document.getElementById("btn-3"); 
+const work4 = document.getElementById("btn-4"); 
+const work5 = document.getElementById("btn-5"); 
+
+work1.addEventListener('click', visitWork1); 
+work2.addEventListener('click', visitWork2); 
+work3.addEventListener('click', visitWork3); 
+work4.addEventListener('click', visitWork4); 
+work5.addEventListener('click', visitWork5); 
+
+function visitWork1(){
+    window.location='https://jemimasiddiqui.github.io/Work-Day-Scheduler-by-Jemima-Siddiqui/Develop/index.html';
+}
+function visitWork2(){
+    window.location='https://jemimasiddiqui.github.io/Password-Generator-by-Jemima-Siddiqui/Develop/index.html';
+}
+function visitWork3(){
+    window.location='https://jemimasiddiqui.github.io/Weather-Dashboard-by-Jemima-Siddiqui/';
+}
+function visitWork4(){
+    window.location='https://book-taku.herokuapp.com/';
+}
+function visitWork5(){
+    window.location='https://jemimasiddiqui.github.io/Holy-Spiritz/';
+}
